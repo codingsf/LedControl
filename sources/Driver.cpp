b555d68986204cc6faae33fe81f5b9c666aa388f
verdandi <aimed.fire@gmail.com>
2014-08-11 18:51:13 +0400

    - Добавлен класс Driver

---
+++ sources/Driver.cpp
@@ -0,0 +1,78 @@
+#include "Driver.h"
+
+#include <cstdlib>
+#include <ctime>
+#include <assert.h>
+
+namespace LedControl {
+
+const std::string Driver::FAIL = "";
+
+Driver::Driver(): state_("off"), color_("blue"), rate_("0") {
+	std::srand(time(0));
+}//end of Driver::Driver()
+
+bool Driver::setState(const std::string& newState) {
+	assert(newState == "on" || newState == "off");
+
+	int fate = std::rand() % MERCY_OF_FATE;
+	if ( fate == 0 ) {
+		return false;
+	}//end of if 
+
+	state_ = newState;
+	return true;
+}//end of bool Driver::setState()
+
+const std::string& Driver::getState() const {
+	int fate = std::rand() % MERCY_OF_FATE;
+	if ( fate == 0 ) {
+		return FAIL;
+	}//end of if 
+
+	return state_;
+}//end of const std::string& Driver::getState()
+
+bool Driver::setColor(const std::string& newColor) {
+	assert(newColor == "green" || newColor == "red" || newColor == "blue");
+
+	int fate = std::rand() % MERCY_OF_FATE;
+	if ( fate == 0 ) {
+		return false;
+	}//end of if 
+
+	color_ = newColor;
+	return true;
+}//end of bool Driver::setColor()
+
+const std::string& Driver::getColor() const {
+	int fate = std::rand() % MERCY_OF_FATE;
+	if ( fate == 0 ) {
+		return FAIL;
+	}//end of if 
+
+	return color_;
+}//end of const std::string& Driver::getColor()
+
+bool Driver::setRate(const std::string& newRate) {
+	assert(newRate == "0" || newRate == "1" || newRate == "2" || newRate == "3" || newRate == "4" || newRate == "5");
+
+	int fate = std::rand() % MERCY_OF_FATE;
+	if ( fate == 0 ) {
+		return false;
+	}//end of if 
+
+	rate_ = newRate;
+	return true;
+}//end of bool Driver::setRate()
+
+const std::string& Driver::getRate() const {
+	int fate = std::rand() % MERCY_OF_FATE;
+	if ( fate == 0 ) {
+		return FAIL;
+	}//end of if 
+
+	return rate_;
+}//end of const str::string& Driver::getRate()
+
+} /* LedControl */ 
---
+++ sources/Driver.h
@@ -0,0 +1,73 @@
+#ifndef LED_CONTROLLER_DRIVER_H_
+#define LED_CONTROLLER_DRIVER_H_
+
+/*
+ * developer: Kuksov Pavel
+ * mail: aimed.fire@gmail.com
+*/
+
+#include <string>
+
+namespace LedControl {
+
+class Driver {
+/*
+ * Класс, релизующий непосредственное управление светодиодом камеры
+ * Данные класс разработан в расчете на то, что в него поступают уже проверенные данные
+*/
+public:
+	Driver(); 
+
+	/*
+	 * включить светодиод
+	 * Принимаемые параметры:
+	 * - newState - новое состояние.
+	 * Может принимать значения "on", "off"
+	*/
+	bool setState(const std::string& newState);
+
+	/*
+	 * запросить состояние светодиода
+	*/
+	const std::string& getState() const;
+
+	/*
+	 * установить цвет светодиода
+	 * Принимаемые параметры:
+	 * - newColor - новый цвет.
+	 * Может принимать значения "red", "green", "blue".
+	 *
+	*/
+	bool setColor(const std::string& newColor);
+
+	/*
+	 * запросить цвет светодиода
+	*/
+	const std::string& getColor() const;
+
+	/*
+	 * установить частоту мерцания светодиода
+	 * Принимаемые параметры:
+	 * - newRate - новая частота в Герцах.
+	 * Может принимать значения "0", "1", "2", "3", "4".
+	*/
+	bool setRate(const std::string& newRate);
+
+	/*
+	 * запросить частоту мерцания светодиода
+	*/
+	const std::string& getRate() const;
+
+	~Driver() {}
+private:
+	static const int MERCY_OF_FATE = 10;
+	static const std::string FAIL;
+	std::string state_;
+	std::string color_;
+	std::string rate_;
+};//end of declaration class Driver
+
+} /* LedControl */ 
+
+#endif /* end of include guard: LED_CONTROLLER_DRIVER_H_ */
+
