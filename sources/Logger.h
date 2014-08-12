d97959cd4ffc353a9392e845bddea0b741eb1237
verdandi <aimed.fire@gmail.com>
2014-08-12 21:40:41 +0400

    - Теперь за добавление сообщения в лог используется функция Logger::addMessage()

--- sources/Logger.h
+++ sources/Logger.h
@@ -11,6 +11,7 @@
 #include <fstream>
 #include <string>
 #include <ctime>
+#include <mutex>
 
 namespace LedControl {
 
@@ -23,6 +24,7 @@ class Logger {
 public:
 #ifdef __LED_CONTROL_TEST__
 	friend class LoggerTest;
+	FRIEND_TEST(LoggerTest, should_add_new_message_in_log);
 	FRIEND_TEST(LoggerTest, should_create_file_in_default_path);
 	FRIEND_TEST(LoggerTest, should_create_file_in_given_path);
 	FRIEND_TEST(LoggerTest, should_open_file_with_saving_previous_data);
@@ -47,23 +49,29 @@ public:
 	/*
 	 * добавить сообщение в лог
 	*/
-	template <class T> Logger& operator<<(const T& message){
-		time_t currentTime;
-		struct tm *timeinfo;
-		std::time(&currentTime);
-		timeinfo = std::localtime(&currentTime);
-		char strTime[100];
-		std::strftime(strTime, 100, "%m-%d-%y %R", timeinfo);
-		logFile_ << '[' << strTime << "] " << message << "\n";
-		return *this;
-	}
+	//template <class T> Logger& operator<<(const T& message){
+		//time_t currentTime;
+		//struct tm *timeinfo;
+		//std::time(&currentTime);
+		//timeinfo = std::localtime(&currentTime);
+		//char strTime[100];
+		//std::strftime(strTime, 100, "%m-%d-%y %R", timeinfo);
+		//logFile_ << '[' << strTime << "] " << message << "\n";
+		//return *this;
+	//}
+	template<class T> void addMessage(const T& message);
 
 private:
 	Logger (const std::string& pathToLogFile);
 
 	std::ofstream logFile_;
+	std::mutex m_;
 };//end of declaration class Logger
 
+template<class T> void Logger::addMessage(const T& message) {
+	std::lock_guard<std::mutex> guard(m_);
+	logFile_ << message;
+}//end of template<class T> void addMessage()
 
 } /* LedControl */ 
 
