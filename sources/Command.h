209f480b4fb1540c24d815492e80217bd6df8048
verdandi <aimed.fire@gmail.com>
2014-08-12 10:05:28 +0400

    - Добавлен интерфейс для классов команд

---
+++ sources/Command.h
@@ -0,0 +1,28 @@
+#ifndef LED_CONTROL_COMMAND_H_
+#define LED_CONTROL_COMMAND_H_
+/*
+ * developer: Kuksov Pavel
+ * mail: aimed.fire@gmail.com
+*/
+
+#include <vector>
+#include <string>
+
+namespace LedControl {
+
+class Command {
+/*
+ * абстрактный класс, представляющий интерфейс для конкретных команд
+*/
+public:
+	virtual ~Command ();
+	virtual bool excute(const std::vector<std::string>& arguments) = 0;
+
+protected:
+	Command ();
+};//end of declaration class Command
+
+} /* LedControl */ 
+
+#endif /* end of include guard: LED_CONTROL_COMMAND_H_ */
+
