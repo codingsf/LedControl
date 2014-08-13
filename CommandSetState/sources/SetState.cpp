e276a7c9311e35e724b177db7d78604377ff1106
verdandi <aimed.fire@gmail.com>
2014-08-13 13:19:06 +0400

    - Объявление класса CommandSetState вынесено в заголовочный файл - функция create объявлена extern "C"

--- CommandSetState/sources/SetState.cpp
+++ CommandSetState/sources/SetState.cpp
@@ -2,43 +2,27 @@
  * developer: Kuksov Pavel
  * mail: aimed.fire@gmail.com
 */
+#include "SetState.h"
 
-#include "../../sources/Command.h"
-#include "../../sources/Driver.h"
-#include "../../sources/Exception.h"
+//namespace LedControl {
 
-namespace LedControl {
+bool CommandSetState::excute(const std::vector<std::string>& arguments) {
+	if ( arguments.size() > 1 ) {
+		throw LedControl::Exception("too many arguments for command 'set-state'");
+	}//end of if 
 
-class CommandSetState: public Command {
-/*
- * класс, реализующий передачу команды драйверу светодиода
-*/
-public:
-	explicit CommandSetState (Driver* reciver) : reciver_(reciver) {}
-	~CommandSetState () {}
+	if ( arguments.empty() ) {
+		throw LedControl::Exception("command 'set-state' need one argument");
+	}//end of if 
 
-	bool excute(const std::vector<std::string>& arguments) {
-		if ( arguments.size() > 1 ) {
-			throw Exception("too many arguments for command 'set-state'");
-		}//end of if 
+	if ( arguments[0] != "on" || arguments[0] != "off" ) {
+		throw LedControl::Exception("bad argument for command 'set-state'");
+	}//end of if 
 
-		if ( arguments.empty() ) {
-			throw Exception("command 'set-state' need one argument");
-		}//end of if 
+	return reciver_->setState(arguments[0]);
+}
 
-		if ( arguments[0] != "on" || arguments[0] != "off" ) {
-			throw Exception("bad argument for command 'set-state'");
-		}//end of if 
 
-		return reciver_->setState(arguments[0]);
-	}
-
-private:
-	Driver* reciver_;
-};//end of declaration class CommandSetState: public Command
-
-Command* create(Driver* reciver) {
-	return new CommandSetState(reciver);
-}//end of Command* create()
-
-} /* LedControl */
+//extern "C" Command* create(LedControl::Driver* reciver) {
+	//return new CommandSetState(reciver);
+//}//end of Command* create()
---
+++ CommandSetState/sources/SetState.h
@@ -0,0 +1,23 @@
+#ifndef LED_CONTROL_COMMAND_SET_STATE_H_
+#define LED_CONTROL_COMMAND_SET_STATE_H_
+
+#include "../../sources/Command.h"
+#include "../../sources/Driver.h"
+#include "../../sources/Exception.h"
+
+#include <iostream>
+
+class CommandSetState: public LedControl::Command {
+/*
+ * класс, реализующий передачу команды драйверу светодиода
+*/
+public:
+	explicit CommandSetState (LedControl::Driver* reciver) : reciver_(reciver) {}
+	~CommandSetState () {}
+
+	virtual bool excute(const std::vector<std::string>& arguments);
+private:
+	LedControl::Driver* reciver_;
+};//end of declaration class CommandSetState: public Command
+
+#endif /* end of include guard: LED_CONTROL_COMMAND_SET_STATE_H_ */
