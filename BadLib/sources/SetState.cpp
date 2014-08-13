f1892c82bf7ea4ff5f2a298f6d223ca7c205ec17
verdandi <aimed.fire@gmail.com>
2014-08-13 17:41:49 +0400

    - Для целей тестирования добавлена намеренно испорченная библиотека

---
+++ BadLib/sources/SetState.cpp
@@ -0,0 +1,23 @@
+/*
+ * developer: Kuksov Pavel
+ * mail: aimed.fire@gmail.com
+*/
+#include "SetState.h"
+
+//namespace LedControl {
+
+bool CommandSetState::excute(const std::vector<std::string>& arguments) {
+	if ( arguments.size() > 1 ) {
+		throw LedControl::Exception("too many arguments for command 'set-state'");
+	}//end of if 
+
+	if ( arguments.empty() ) {
+		throw LedControl::Exception("command 'set-state' need one argument");
+	}//end of if 
+
+	if ( arguments[0] != "on" || arguments[0] != "off" ) {
+		throw LedControl::Exception("bad argument for command 'set-state'");
+	}//end of if 
+
+	return reciver_->setState(arguments[0]);
+}
