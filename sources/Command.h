4adab353f2f68e0cd8bb58a3b69a5c6e331bb703
verdandi <aimed.fire@gmail.com>
2014-08-13 13:13:06 +0400

    - Конструктор и деструктор в абстрактном классе Command были не определены

--- sources/Command.h
+++ sources/Command.h
@@ -15,14 +15,14 @@ class Command {
  * абстрактный класс, представляющий интерфейс для конкретных команд
 */
 public:
-	virtual ~Command ();
+	virtual ~Command (){}
 	virtual bool excute(const std::vector<std::string>& arguments) = 0;
 
 protected:
-	Command ();
+	Command (){}
 };//end of declaration class Command
 
-} /* LedControl */ 
+} // LedControl
 
 #endif /* end of include guard: LED_CONTROL_COMMAND_H_ */
 
