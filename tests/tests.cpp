20ab2691b277917567ad5dd2e4743e85969134d4
verdandi <aimed.fire@gmail.com>
2014-08-14 08:08:59 +0400

    - Подключили тесты для MessageHandler

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,7 @@
 #include "ExceptionTest.h"
 #include "LoggerTest.h"
 #include "CommandFactoryTest.h"
+#include "MessageHandlerTest.h"
 
 int main(int argc, char *argv[]) {
 	::testing::InitGoogleTest(&argc, argv);
