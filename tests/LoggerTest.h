a1f3a9035c829b58de0c465c733fb1547525a1c8
verdandi <aimed.fire@gmail.com>
2014-08-14 16:58:41 +0400

    - В тесте should_create_file_in_given_path добавлена проверка того, что файл действительно удаляется

--- tests/LoggerTest.h
+++ tests/LoggerTest.h
@@ -40,6 +40,8 @@ TEST_F(LoggerTest, should_create_file_in_given_path){
 	}
 
 	std::remove("/tmp/log.txt");
+	struct stat sb;
+	EXPECT_TRUE(stat("/tmp/log.txt", &sb) != 0);
 }
 
 TEST_F(LoggerTest, should_open_file_with_saving_previous_data){
