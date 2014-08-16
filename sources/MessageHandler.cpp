34d689e233b6bd4b65a1ef872620ea0dc45e73f5
verdandi <aimed.fire@gmail.com>
2014-08-16 09:16:52 +0400

    - Временно убраны тесты

--- sources/MessageHandler.cpp
+++ sources/MessageHandler.cpp
@@ -7,6 +7,9 @@ namespace LedControl {
 const std::string MessageHandler::PREFIX = "/tmp/";
 
 MessageHandler::MessageHandler(const std::string& serverName, CommandFactory* cf, Logger* log): cf_(cf), log_(log), isFinished_(false) {
+	log_->addMessage("server starting...", Logger::ADD_TIME | Logger::ADD_LN);
+
+	//создаем файл fifo, если его еще нет и открываем его
 	std::string pathToFifo = PREFIX + serverName;
 	struct stat sb;
 	if ( ::stat(pathToFifo.c_str(), &sb) != 0 ) {
@@ -19,7 +22,23 @@ MessageHandler::MessageHandler(const std::string& serverName, CommandFactory* cf
 	if(mainDesc_ < 0) {
 		throw Exception(Exception::getSystemErrorMessage());
 	}
+
+	log_->addMessage("server started!", Logger::ADD_TIME | Logger::ADD_LN);
+
+	//устанавливаем способы обработки сигналов
+	//sigset_t sset;
+	//::sigfillset(&sset);
+	//::sigdelset(&sset, SIGINT);
+	//::sigprocmask(SIG_SETMASK, &sset, 0);
+	//struct sigaction sa;
+	//SignalHandler sh(this);
+	////sa.sa_handler = &sh;
+	//::sigaction(SIGINT, &sa, 0);
 }//end of MessageHandler::MessageHandler()
+
+void MessageHandler::getRequest() {
+
+}//end of void MessageHandler::getRequest()
 
 MessageHandler::~MessageHandler() {
 	::close(mainDesc_);
--- sources/MessageHandler.h
+++ sources/MessageHandler.h
@@ -17,6 +17,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <signal.h>
 
 namespace LedControl {
 
@@ -29,18 +30,42 @@ class MessageHandler {
 		std::string pid_; //идентификатор клиента
 		std::string identifier_; //идентификатор команды
 		std::vector<std::string> arguments_;
-	};
+	}; //end of struct Task
+
+	class SignalHandler {
+	public:
+		SignalHandler (MessageHandler* mh): mh_(mh) {}
+		~SignalHandler () {}
+
+		//void operator()(int val){
+			//mh_->finishWork();
+			//val = 0;
+		//}
+	
+	private:
+		MessageHandler* mh_;
+	};//end of declaration class SignalHandler
 
 public:
+#ifdef __LED_CONTROL_TEST__
+	friend class MessageHandlerTest;
+	FRIEND_TEST(MessageHandlerTest, should_read_messages_from_pipe_and_close_connection_on_fifo_if_recive_SIGINT);
+#endif
+
 	MessageHandler (const std::string& serverName, CommandFactory* cf, Logger* log);
 	~MessageHandler ();
 
+	void getRequest();
+
+	void finishWork(){
+		isFinished_ = true;
+	}
 private:
 	static const std::string PREFIX;
 
 	CommandFactory* cf_;
 	Logger* log_;
-	bool isFinished_; //true - если работа закончена.
+	volatile bool isFinished_; //true - если работа закончена.
 	int mainDesc_; //дескриптор главного канала. Именно в этот канал клиенты будут посылать сообщения
 	MessageQueue<Task> queue_;
 };//end of declaration class MessageHandler
